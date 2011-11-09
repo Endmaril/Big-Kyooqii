50a2c9de27e1d80a3890c09cf02c8ed71f8cc691
Wiz <wiz@haxx.es>
2011-11-09 11:46:56 +0100

    Bases of the framework

---
+++ js/Application.js
@@ -0,0 +1,66 @@
+/*
+    This file is part of the Big Kyoqii game.
+    Copyright (C) 2011  Jérémy Gabriele, Romaric Pighetti, Wiz
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+var Application = new Class({
+    scene: null,
+    context: null,
+    canvas: null,
+
+    initialize: function(){
+        this.canvas = document.getElementById('game-canvas');
+        this.context = this.canvas.getContext('2d');
+
+        this.setScene(new Title(this));
+
+        document.addEvent('keydown', function(event){
+            if(!this.scene.keyDown(event))
+                event.preventDefault();
+        }.bind(this));
+
+        this.invalidate();
+    },
+
+    setScene: function(scene)
+    {
+        this.scene = scene;
+    },
+
+    invalidate: function(){
+        var pThis = this;
+        window.requestAnimFrame(function(time){
+            pThis.render(time);
+        });
+    },
+
+    render: function(time){
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.scene.render(this.context, time);
+    },
+});
+
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       || 
+            window.webkitRequestAnimationFrame || 
+            window.mozRequestAnimationFrame    || 
+            window.oRequestAnimationFrame      || 
+            window.msRequestAnimationFrame     || 
+            function(callback){
+                window.setTimeout(callback, 1000 / 60);
+            };
+})();
--- js/scenes/Title.js
+++ js/scenes/Title.js
@@ -18,21 +18,80 @@
 
 var Title = new Class({
   
-  Extends: Scene,
+    Extends: Scene,
+    menus: [],
+    selected: 0,
   
-  initialize: function()
-  {
-    
-  },
+    initialize: function(app)
+    {
+        this.app = app;
+        this.canvas = app.canvas;
+
+        this.menus.push({text: 'Play', callback: function(){
+            app.setScene(new Game());
+        }});
+        this.menus.push({text: 'Options', callback: function(){
+            app.SetScene(new TitleOptions());
+        }});
+        this.menus.push({text: 'Help', callback: function(){
+            app.SetScene(new TitleHelp());
+        }});
+    },
   
-  update: function()
-  {
-    
-  },
+    update: function()
+    {
+
+    },
+
+    keyDown: function(event)
+    {
+        if(event.key == 'up')
+        {
+            if(this.selected > 0)
+                this.selected--;
+
+            this.app.invalidate();
+            return false;
+        }
+        else if(event.key == 'down')
+        {
+            if(this.selected < this.menus.length - 1)
+                this.selected++;
+
+            this.app.invalidate();
+            return false;
+        }
+
+        return true;
+    },
   
-  render: function()
-  {
-    
-  }
-  
+    render: function(ctx, time)
+    {
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'top';
+        ctx.font = '40pt Courier';
+        ctx.fillStyle = 'rgb(255, 0, 0)';
+        ctx.fillText("Big-Kyooqii", this.canvas.width / 2, 13.37);
+
+        ctx.textAlign = 'left';
+        ctx.textBaseline = 'middle';
+        ctx.font = '20pt Courier';
+
+        var x = this.canvas.width / 3;
+        var y = 240;
+        this.menus.each(function(menu, index){
+            ctx.fillStyle = 'rgb(0, 0, 0)';
+            ctx.fillText(menu.text, x, y);
+            if(index == this.selected)
+            {
+                ctx.beginPath();
+                ctx.arc(x - 40, y, 10, 0, 2 * Math.PI, false);
+                ctx.fillStyle = 'rgb(0, 0, 255)';
+                ctx.fill();
+            }
+
+            y += 40;
+        }, this);
+    }
+
 });

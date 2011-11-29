b295f1c01020283a7a9541eb3c499d559ebdcd40
Romaric Pighetti <romaric.pighetti@gmail.com>
2011-11-29 23:05:19 +0100

    Credit done

--- js/scenes/Credit.js
+++ js/scenes/Credit.js
@@ -25,7 +25,52 @@ var Credit = new Class({
         this.parent(app);
         
         this.objects.text1 = new TextObject({
-            content: 'Développeurs:\n \t Jérémy Gabriele'
+            content: 'Développeurs:'
+        });
+        
+        this.objects.text2 = new TextObject({
+            content: 'Jérémy Gabriele',
+            x:280
+        });
+        
+        this.objects.text3 = new TextObject({
+            content: 'Clément Léger (Wiz)',
+            x:280,
+            y:100
+        });
+        
+        this.objects.text4 = new TextObject({
+            content: 'Romaric Pighetti',
+            x:280,
+            y:150
+        });
+        
+        this.objects.text5 = new TextObject({
+            content: 'Graphisme: ',
+            y:200
+        });
+        
+        this.objects.text6 = new TextObject({
+            content: 'Anthony Germain',
+            x:280,
+            y:200
+        });
+        
+        this.objects.text7 = new TextObject({
+            content: 'www.lostgarden.com',
+            x:280,
+            y:250
+        });
+        
+        this.objects.text8 = new TextObject({
+            content: 'GameDesign:',
+            y:300
+        });
+        
+        this.objects.text9 = new TextObject({
+            content: 'Paul Sanchez',
+            y:300,
+            x:280
         });
     },
 

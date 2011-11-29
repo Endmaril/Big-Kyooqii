440e9cbe668bcc2234b808e3636b632e837e39f2
Romaric Pighetti <romaric.pighetti@gmail.com>
2011-11-29 23:46:07 +0100

    Rédaction de l'aide

--- js/scenes/TitleHelp.js
+++ js/scenes/TitleHelp.js
@@ -25,8 +25,54 @@ var TitleHelp = new Class({
         this.parent(params);
         
         this.objects.text1 = new TextObject({
-            content: 'Fucking help'
+            content: 'Vous devez trouver la clé pour pouvoir vous '
         });
+        
+        this.objects.text2 = new TextObject({
+            content: 'échaper du donjon. Vous possédez une lampe ',
+            y:100
+        });
+        
+        this.objects.text3 = new TextObject({
+            content: 'qui s\'éteind progressivement. Vous devrez ',
+            y:150
+        });
+        
+        this.objects.text4 = new TextObject({
+            content: 'trouver du carburant pour l\'alimenter si ',
+            y:200
+        });
+        
+        this.objects.text5 = new TextObject({
+            content: 'vous ne voulez pas qu\'elle s\'éteigne. ',
+            y:250
+        });
+        
+        this.objects.text6 = new TextObject({
+            content: 'Faite attention aux trolls, ils sont ',
+            y:300
+        });
+        
+        this.objects.text7 = new TextObject({
+            content: 'dangereux. En revanche les bairks ',
+            y:350
+        });
+        
+        this.objects.text8 = new TextObject({
+            content: 'requierent votre aide. Cependant, ils',
+            y:400
+        });
+        
+        this.objects.text9 = new TextObject({
+            content: 'ont peur dès que quiconque approche,',
+            y:450
+        });
+        
+        this.objects.text10 = new TextObject({
+            content: 'il faudra donc les rattraper.',
+            y:500
+        });
+        
     },
 
     keyDown: function(event)
